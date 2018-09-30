cf0eec4ec3c60b2f4b0ad3286abe58340aaf43c9
wasabi315 <wasabi.nvim.st@gmail.com>
2018-10-01 06:21:38 +0900

    change xmonad config

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,42 +1,50 @@
+{-# LANGUAGE TupleSections #-}
+
 --
 --                   __  ____  __                  _
 --                   \ \/ /  \/  |___ _ _  __ _ __| |
 --                    >  <| |\/| / _ \ ' \/ _` / _` |
 --                   /_/\_\_|  |_\___/_||_\__,_\__,_|
 --
+--       XMonad config file (https://github.com/wasabi315/dotfiles)
+--
 
+import qualified Data.Map    as M
 import           Data.Monoid
-import qualified Data.Map        as M
 import           System.Exit
 
 import           XMonad
-import qualified XMonad.StackSet as W
+import qualified XMonad.StackSet    as W
+
+import           XMonad.Actions.Submap
 
 import           XMonad.Config.Desktop
 
-import           XMonad.Hooks.DynamicLog
 import           XMonad.Hooks.InsertPosition
 import           XMonad.Hooks.ManageDocks
 
+import           XMonad.Layout.Gaps
 import           XMonad.Layout.Spacing
 
 -------------------------------------------------------------------------------
 
-main = xmonad =<< xmobar myConfig
+main = xmonad myConfig
 
 myConfig = desktopConfig
-    { terminal           = myTerminal
-    , focusFollowsMouse  = myFocusFollowsMouse
-    , clickJustFocuses   = myClickJustFocuses
-    , borderWidth        = myBorderWidth
-    , modMask            = myModMask
-    , workspaces         = myWorkspaces
-    , keys               = myKeys
-    , layoutHook         = myLayout
-    , manageHook         = myManageHook
-    , handleEventHook    = myEventHook
-    , logHook            = myLogHook
-    , startupHook        = myStartupHook
+    { terminal            = myTerminal
+    , focusFollowsMouse   = myFocusFollowsMouse
+    , clickJustFocuses    = myClickJustFocuses
+    , normalBorderColor   = myNormalBorderColor
+    , focusedBorderColor  = myFocusedBorderColor
+    , borderWidth         = myBorderWidth
+    , modMask             = myModMask
+    , workspaces          = myWorkspaces
+    , keys                = myKeys
+    , layoutHook          = myLayout
+    , manageHook          = myManageHook
+    , handleEventHook     = myEventHook
+    , logHook             = myLogHook
+    , startupHook         = myStartupHook
     }
 
 -------------------------------------------------------------------------------
@@ -44,8 +52,10 @@ myConfig = desktopConfig
 myTerminal = "termite"
 
 myFocusFollowsMouse = False
+myClickJustFocuses  = False
 
-myClickJustFocuses = False
+myNormalBorderColor  = "#fff8f5"
+myFocusedBorderColor = "#e5ddcb"
 
 myBorderWidth = 8
 
@@ -53,14 +63,13 @@ myModMask = mod1Mask
 
 myWorkspaces = ["1: term", "2: web", "3: drawing"] ++ map show [4..9]
 
--- Key bindings
 myKeys conf@(XConfig { modMask = mod }) = M.fromList $
 
     -- launch a terminal
     [ ((mod,               xK_Return), spawn $ terminal conf)
 
     -- launch dmenu
-    , ((mod,               xK_p     ), spawn "rofi -show drun")
+    , ((controlMask,       xK_semicolon), spawn "rofi -show drun")
 
     -- close focused window
     , ((mod .|. shiftMask, xK_q     ), kill)
@@ -81,19 +90,19 @@ myKeys conf@(XConfig { modMask = mod }) = M.fromList $
     , ((mod,               xK_j     ), windows W.focusDown)
 
     -- Move focus to the previous window
-    , ((mod,               xK_k     ), windows W.focusUp  )
+    , ((mod,               xK_k     ), windows W.focusUp)
 
     -- Move focus to the master window
-    , ((mod,               xK_m     ), windows W.focusMaster  )
+    , ((mod,               xK_m     ), windows W.focusMaster)
 
     -- Swap the focused window and the master window
     , ((mod .|. shiftMask, xK_Return), windows W.swapMaster)
 
     -- Swap the focused window with the next window
-    , ((mod .|. shiftMask, xK_j     ), windows W.swapDown  )
+    , ((mod .|. shiftMask, xK_j     ), windows W.swapDown)
 
     -- Swap the focused window with the previous window
-    , ((mod .|. shiftMask, xK_k     ), windows W.swapUp    )
+    , ((mod .|. shiftMask, xK_k     ), windows W.swapUp)
 
     -- Shrink the master area
     , ((mod,               xK_h     ), sendMessage Shrink)
@@ -119,39 +128,32 @@ myKeys conf@(XConfig { modMask = mod }) = M.fromList $
     ]
     ++
 
-    [((m .|. mod, k), windows $ f i)
+    [ ((m .|. mod, k), windows $ f i)
         | (i, k) <- zip (workspaces conf) [xK_1 .. xK_9]
-        , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
+        , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)] ]
     ++
 
-    [((m .|. mod, key), screenWorkspace sc >>= flip whenJust (windows . f))
+    [ ((m .|. mod, key), screenWorkspace sc >>= flip whenJust (windows . f))
         | (key, sc) <- zip [xK_w, xK_e, xK_r] [0..]
-        , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
+        , (f, m) <- [(W.view, 0), (W.shift, shiftMask)] ]
 
--- Layouts
 myLayout = tiled ||| Mirror tiled ||| Full
   where
-     tiled  = spacing 15 $ Tall 1 (1/20) (1/2)
+     tiled  = spacing myGap . myGaps $ Tall 1 (1/20) (1/2)
+     myGaps = gaps (map (, myGap) [U .. L])
+     myGap  = 5
 
--- Window rules
-myManageHook = composeAll
-    [ className =? "Google-chrome"  --> doShift (myWorkspaces !! 1)
+myManageHook = insertPosition End Newer <+> composeAll
+    [ className =? "Termite"        --> doShift (myWorkspaces !! 0)
+    , className =? "Google-chrome"  --> doShift (myWorkspaces !! 1)
+    , className =? "GravitDesigner" --> doShift (myWorkspaces !! 2)
     , resource  =? "desktop_window" --> doIgnore
-    , not <$> isFloat               --> insertPosition End Newer
     ]
-  where
-    isFloat = liftX $ do
-        floats <- gets $ W.floating . windowset
-        ws     <- gets $ W.integrate' . W.stack .W.workspace . W.current . windowset
-        return $ any (flip M.member floats) ws
 
--- Event handling
 myEventHook = mempty
 
--- Status bars and logging
 myLogHook = return ()
 
--- Startup hook
 myStartupHook = mapM_ spawn
     [ "compton"
     , "~/.fehbg"
