e2420a12c34581be8dc99347914b496d3571f861
wasabi315 <wasabi.nvim.st@gmail.com>
2019-03-28 22:16:39 +0900

    commit

--- setup.sh
+++ setup.sh
@@ -4,11 +4,11 @@ if [ -z "${DOTPATH:-}" ]; then
     DOTPATH="${HOME}/.dotfiles"; export DOTPATH
 fi
 
-download_dotfiles() {
+GITHUB_URL="https://github.com/wasabi315/dotfiles.git"
+TARBALL="https://github.com/wasabi315/dotfiles/tarball/master"
 
-    local readonly git_tarball="https://github.com/wasabi315/dotfiles/tarball/master"
 
-    cat <<HEADER
+cat <<HEADER
  ___   ___ _____ ___ ___ _    ___ ___
 |   \ / _ \_   _| __|_ _| |  | __/ __|
 | |) | (_) || | | _| | || |__| _|\___\\
@@ -16,30 +16,26 @@ download_dotfiles() {
 
 HEADER
 
-    if [ -d "${DOTPATH}" ]; then
-        echo "$(tput bold)$(tput setaf 1)ERROR: dotfiles is already installed on this system.$(tput sgr0)\n" >&2
-        exit 1
-    fi
+if [ -d "${DOTPATH}" ]; then
+    echo "$(tput bold)$(tput setaf 1)ERROR: dotfiles is already installed on this system.$(tput sgr0)" >&2
+    exit 1
+fi
 
-    echo "$(tput setaf 1)This will download dotfiles to ~/.dotfiles directory.$(tput sgr0)\n"
-    echo "$(tput setaf 1)Continue? (y/n)$(tput sgr0): "
-    read -r -p "[Y|n]" response
-    if [[ $response =~ ^(yes|y|Y)]]; then
-        echo "$(tput setaf 7)Downloading dotfiles...$(tput sgr0)"
-    else
-        echo "$(tput bold)$(tput setaf 7)Process terminated by user.$(tput sgr0)\n"
-        exit 1
-    fi
+echo "$(tput setaf 1)This will download dotfiles to ~/.dotfiles directory.$(tput sgr0)"
+echo "$(tput setaf 1)Continue? (y/n)$(tput sgr0): "
+read -r -p "[Y|n]" response
+if [[ $response =~ ^(yes|y|Y) ]]; then
+    echo "$(tput setaf 7)Downloading dotfiles...$(tput sgr0)"
+else
+    echo "$(tput bold)$(tput setaf 7)Process terminated by user.$(tput sgr0)\n"
+    exit 1
+fi
 
-    mkdir ${DOTPATH}
+mkdir ${DOTPATH}
 
-    wget -qO ${HOME}/dotfiles.tar.gz ${git_tarball}
-    tar -xzvf ${HOME}/dotfiles.tar.gz -C ${DOTPATH}
-    rm ${HOME}/dotfiles.tar.gz
+wget -qO ${HOME}/dotfiles.tar.gz ${git_tarball}
+tar -xzvf ${HOME}/dotfiles.tar.gz -C ${DOTPATH}
+rm ${HOME}/dotfiles.tar.gz
 
-    echo "$(tput bold)$(tput setaf 2)Done$(tput sgr0)\n"
-
-}
-
-download_dotfiles
+echo "$(tput bold)$(tput setaf 2)Done$(tput sgr0)\n"
 
