6da6c821e3eb9d7b55b4f7c54784c1f3ff7bb075
Satoshi Takimoto <wasabi.nvim.st@gmail.com>
2019-04-30 17:52:18 +0900

    change xmonad configs

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -13,11 +13,10 @@ import           Data.Monoid
 import           XMonad
 import qualified XMonad.StackSet as W
 
-import           XMonad.Actions.CopyWindow
-
 import           XMonad.Config.Desktop
 
 import           XMonad.Hooks.DynamicLog
+import           XMonad.Hooks.InsertPosition
 import           XMonad.Hooks.ManageDocks
 import           XMonad.Hooks.ManageHelpers
 
@@ -53,21 +52,20 @@ black  = "#2e3440"
 -------------------------------------------------------------------------------
 -- Keybindings
 
-setMyKeys :: XConfig a -> XConfig a
-setMyKeys conf = conf
-  `additionalKeysP`
-    [ ("M-a", spawn "rofi -show drun")
+myKeys =
+    [ ("C-;",        spawn "rofi -show drun")
     , ("M-<Return>", spawn myTerminal)
-    , ("M-h", sendMessage (Go L))
-    , ("M-j", sendMessage (Go D))
-    , ("M-k", sendMessage (Go U))
-    , ("M-l", sendMessage (Go R))
-    , ("M-S-h", sendMessage (Swap L))
-    , ("M-S-j", sendMessage (Swap D))
-    , ("M-S-k", sendMessage (Swap U))
-    , ("M-S-l", sendMessage (Swap R))
-    , ("M-c", kill)
-    , ("M-S-r", restart "xmonad" True)
+    , ("M-h",        sendMessage (Go L))
+    , ("M-j",        sendMessage (Go D))
+    , ("M-k",        sendMessage (Go U))
+    , ("M-l",        sendMessage (Go R))
+    , ("M-S-h",      sendMessage (Swap L))
+    , ("M-S-j",      sendMessage (Swap D))
+    , ("M-S-k",      sendMessage (Swap U))
+    , ("M-S-l",      sendMessage (Swap R))
+    , ("M-x",        kill)
+    , ("M-S-r",      restart "xmonad" True)
+    , ("<Print>",    spawn "scrot -e 'mv $f ~/Pictures/scrots'")
     ]
 
 -------------------------------------------------------------------------------
@@ -78,16 +76,17 @@ myLayout = windowNavigation . gaps myGaps . spacing spacingWidth
     ||| Mirror tiled
     ||| Full
   where
-    tiled = Tall 1 (3 / 100) (1 / 2)
+    tiled  = Tall 1 (3 / 100) (1 / 2)
     myGaps = zip [U .. L] (repeat gapWidth)
 
 -------------------------------------------------------------------------------
 -- Window rules
 
-myManageHook = composeAll
+myManageHook = insertPosition End Newer <+> composeAll
     [ className =? "Termite"       --> doShift "1"
     , className =? "Google-chrome" --> doShift "2"
     , className =? "feh"           --> doCenterFloat
+    , isDialog                     --> doCenterFloat
     ]
 
 -------------------------------------------------------------------------------
@@ -105,17 +104,17 @@ myLogHook h = dynamicLogWithPP myPP { ppOutput = hPutStrLn h }
 myPP = xmobarPP
     { ppOrder   = \(ws:_:t:_) -> [ws, t]
     , ppTitle   = xmobarColor green "" . shorten 40
-    , ppCurrent = xmobarColor black blue . pad
-    , ppUrgent  = xmobarColor black red . pad
-    , ppVisible = pad
-    , ppHidden  = pad
+    , ppCurrent = xmobarColor black blue . pad . pad
+    , ppUrgent  = xmobarColor black red . pad . pad
+    , ppVisible = pad . pad
+    , ppHidden  = pad . pad
     , ppWsSep   = ""
     , ppSep     = "  "
     }
 
 -------------------------------------------------------------------------------
 
-myConfig = setMyKeys desktopConfig
+myConfig = desktopConfig
     { terminal           = myTerminal
     , focusFollowsMouse  = myFocusFollowsMouse
     , modMask            = myModMask
@@ -127,6 +126,8 @@ myConfig = setMyKeys desktopConfig
     , manageHook         = myManageHook
     , startupHook        = myStartupHook
     }
+  `additionalKeysP`
+    myKeys
 
 -------------------------------------------------------------------------------
 
